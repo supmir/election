bc4482d55018459215e425cc58b0bee1eecb152e
Amir <iskandar.amir@gmail.com>
2025-08-14 08:46:49 +0800

    New detail card design

--- components/detailCard.tsx
+++ components/detailCard.tsx
@@ -33,18 +33,29 @@ export default function DunDetailCard(props: DunDetailCardProps) {
 
     >
         <div className="flex flex-col gap-4 py-4">
-            <div className="gap-2 flex">
-                <span className="text-xl font-bold">{dunDetails.dunCode}</span>
-                <span className="text-lg">
-                    {dunDetails.dunShortName}
-                </span>
+            <div className="flex flex-col">
+                <div className="gap-2 flex">
+                    <span className="text-xl font-bold">{dunDetails.dunCode}</span>
+                    <span className="text-lg">
+                        {dunDetails.dunShortName}
+                    </span>
+                </div>
+                <div className="gap-2 flex">
+                    <span className="text-lg font-bold">Jumlah Undi</span>
+                    <span className="text-lg">
+                        {sortedCandidates.reduce((prev, val) => { return prev + val.votes; }, 0)}
+                    </span>
+                </div>
             </div>
             <div className="flex flex-col gap-2">
                 {sortedCandidates.map((candidateDetails, i) => {
                     return <div key={i} className="flex justify-between">
                         <div className="flex flex-col gap-1">
-                            <div className="text-sm">{candidateDetails.candidateSequence}. {candidateDetails.candidateName}</div>
                             <div className="font-bold">{candidateDetails.partyCode}</div>
+                            <div className="text-sm">
+                                {/* {candidateDetails.candidateSequence}.  */}
+                                {candidateDetails.candidateName}
+                            </div>
                         </div>
                         <div>{candidateDetails.votes}</div>
                     </div>;
