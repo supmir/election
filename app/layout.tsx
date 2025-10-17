f4d191dd42d8af43bed3bc8fe1f7eafbeed71177
Amir <iskandar.amir@gmail.com>
2025-10-17 20:58:20 +0800

    Hide scrollbar better

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,9 +23,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="no-scrollbar">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased no-scrollbar`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
